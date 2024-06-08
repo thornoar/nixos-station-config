f174b2e44bf49e7d8dc2dd7c9334604a2bb2fa3d
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-06-09 01:50:24 +0200

    moving value setting to the local side

--- home-local.nix
+++ home-local.nix
@@ -2,10 +2,25 @@
 
 {
     config = {
-        wallpaperDir = "Landscapes";
-        fontsizeBar = 13;
         fontsize = 11;
+        fontsizeBar = 13;
+
+        wallpaperDir = "Landscapes";
+        wallpaperGamma = 0.9;
+        wallpaperContrast = 1.0;
+
+        windowSpace = 14;
+        windowBorderWidth = 0;
+
+        terminalOpacity = 0.9;
+        terminalPadding = { x = 4; y = 3; };
+
+        barHeight = 35;
+
+        magnifiedScale = 1.5;
+
         scratchpadWidth = lib.mkForce "19 % 30";
         scratchpadHeight = lib.mkForce "39 % 60";
+        font = "Hack";
     };
 }
